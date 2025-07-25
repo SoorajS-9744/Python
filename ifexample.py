ab1548051e613818b572212df762864e8a808dac
sooraj <sooraj@GMIMPEX.IN>
2025-07-25 18:14:50 +0530

    python push

--- ifexample.py
+++ ifexample.py
@@ -1,10 +1,71 @@
 #symbro task
 
-a = 5
-b = 2
+# addition / Subtration / multilipcation / division
 
-power = a ** b 
-print(power)
+# a = 5 
+# b = 3
+# sum = a+b  (+,-,*,/)
+# print(a,'+',b,'=',sum)
+#-------------------------------------------------------------------End
 
+# if statement with input(give values)
 
+# Name = 'Vishnu'
+# Maths = int(input('Enter maths mark:'))
+# hindi = int(input('Enter hindi mark:'))
+# minimum_mark = 50
 
+# if Maths>=minimum_mark:
+#     print('Vishnu pass')
+# elif hindi>=minimum_mark:
+#     print('Vishnu pass')
+# else:
+#     print("Vishnu Failed")  
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,(2)
+# a = int(input('enter a number :'))   
+# b = int(input('enter a number :'))
+# sum = a + b
+# print('Result :',a,'+',b,'=',sum)   
+#--------------------------------------------------------------------End
+
+# using while loop 
+
+# a = 1
+# b = 5
+# while a <= b:
+#     print('Hello')
+#     a += 1
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,(2)
+
+# using for loop
+
+# a = ('Apple','Orange','Banana','Grape')
+
+# for i in a :
+#     print(i)
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,(3)  
+
+# a = range(5)   # will loop from 0 to 4
+
+# for i in a:
+#     print(i)
+
+#---------------------------------------------------------------------End
+     
+# using break and continue
+
+# a = (1,2,3,4,0,5,6,7)
+
+# for i in a:
+#     if i == 0:
+#         break;
+#     print(i)       
+
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,(2)
+
+# s = (1,2,3,4,0,5,6,7)
+
+# for i in s:
+#     if i == 0:
+#         continue;
+#     print(i)
