72d63b5c5118b27a75a1868ec5b00bd353b88913
sooraj <sooraj@GMIMPEX.IN>
2025-07-26 18:15:55 +0530

    task 1

---
+++ task1.py
@@ -0,0 +1,81 @@
+# que 1
+
+# city = 'Banglore'
+# print(city)
+
+
+#que 2
+
+# lenght = 7
+# width = 5
+# area = lenght*width
+# print('area =',area)
+
+
+#que 3
+
+# x = 25
+# y = 30
+# print(x == y)
+# print(x != y)
+# print(x <= y)
+# print(x >= y)
+# print(x < y)
+# print(x > y)
+
+
+#que 4
+
+# a = 'apple'
+# b = 'Apple'
+# print(a == b)
+
+
+#que 5
+
+# a = 0
+# print(a)
+
+# a += 5
+# print(a)
+
+# a /= 2
+# print(a)
+
+
+#que 6
+
+# a = int(input('Enter a number :'))
+
+# a += 10
+# print(a)
+
+# a -= 3
+# print(a)
+
+
+#que 7
+
+# a = [1,2,3]
+# b = a
+
+# print(a is b)
+
+
+#que 8
+
+# x = [10,20]
+# y = [10,20]
+
+# print(x == y)
+# print(x is y)
+
+
+#que 9
+
+# x = [10,20]
+# y = [10,20]
+
+# print(x is not y)
+
+
